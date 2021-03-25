61788afe4c26bc9466c56a1b56a09f5566b6d951
guanzhenguo@3k.com <guanzhenguo@3k.com>
2021-03-25 17:26:15 +0800

    123

--- test.php
+++ test.php
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-
-test master
-=======
-test2 master
->>>>>>> test2
-=======
-test master1
-test master2
->>>>>>> Stashed changes
-=======
-hello world
->>>>>>> yy
-12345
-abcde
+123
